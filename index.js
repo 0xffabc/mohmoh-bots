d7f6419d2611a76c8955b7a820da17faf28ea20f
0xffabc <139798156+0xffabc@users.noreply.github.com>
2023-10-02 08:50:03 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
+const { WebSocket } = require("ws");
+const { encode, decode } = require("msgpack-lite");
+const ws = new WebSocket("wss://mohmoh.onrender.com/");ws.binaryType = "arraybuffer";
+function emit(...a) {
+  const name = a.shift();
+  const packet = encode([name, a]);
+  ws.send(packet);
+}
+ws.onmessage = function(e) {
+  const [packet, data] = decode(e.data);
+  console.log(packet, data);
+  switch(packet) {
+    case "io-init":
+      emit("sp", {name: "ZoleSBots", skin: "__proto__", moofoll: 1});
+      break;
+    case "id":
+      pid = data[0];
+      break;
+    case "33":
+      emit("c", 1, Number.MAX_VALUE);
+      emit("33", Number.MAX_VALUE);
+      break;
+    case "h":
+      setTimeout(() => {
+        let i = 3;
+        while (i--) {
+          emit("5", 0, false);
+          emit("c", 1);
+          emit("5", 0, true);
+        };
+      });
+      break;
+  }
+}
 
